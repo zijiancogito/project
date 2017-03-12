4ff0baa828b2410ddf621a72f53b0f761a555fd6
cogito <345981440@qq.com>
2017-03-12 22:36:07 +0800

    register

--- pages/register/register.js
+++ pages/register/register.js
@@ -1,9 +1,51 @@
 Page({
+    data: {
+        checkboxItems: [
+            {name: 'standard is dealt for u.', value: '0', checked: true},
+            {name: 'standard is dealicient for u.', value: '1'}
+        ],
+
+        countryCodes: ["+86", "+80", "+84", "+87"],
+        countryCodeIndex: 0,
+
+        isAgree: false
+    },
     toProtocol:function(){
         this.gopage("../protocol/protocol")
     },
     reg:function(){
-        this.gopage("../dialogList/dialogList")
+        this.gopage("../index/index")
+    },
+    bindAgreeChange: function (e) {
+        this.setData({
+            isAgree: !!e.detail.value.length
+        });
+    },
+    bindCountryCodeChange: function(e){
+        console.log('picker country code 发生选择改变，携带值为', e.detail.value);
+
+        this.setData({
+            countryCodeIndex: e.detail.value
+        })
+    },
+    checkboxChange: function (e) {
+        console.log('checkbox发生change事件，携带value值为：', e.detail.value);
+
+        var checkboxItems = this.data.checkboxItems, values = e.detail.value;
+        for (var i = 0, lenI = checkboxItems.length; i < lenI; ++i) {
+            checkboxItems[i].checked = false;
+
+            for (var j = 0, lenJ = values.length; j < lenJ; ++j) {
+                if(checkboxItems[i].value == values[j]){
+                    checkboxItems[i].checked = true;
+                    break;
+                }
+            }
+        }
+
+        this.setData({
+            checkboxItems: checkboxItems
+        });
     },
     gopage:function(url){
     wx.navigateTo({
