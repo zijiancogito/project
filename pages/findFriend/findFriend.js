2f62c1d92d316f8516361b07f236cbc0f0c7a973
cogito <345981440@qq.com>
2017-03-16 19:38:50 +0800

    finfriend

--- pages/findFriend/findFriend.js
+++ pages/findFriend/findFriend.js
@@ -1,4 +1,29 @@
 Page({
+    data: {
+        inputShowed: false,
+        inputVal: ""
+    },
+    showInput: function () {
+        this.setData({
+            inputShowed: true
+        });
+    },
+    hideInput: function () {
+        this.setData({
+            inputVal: "",
+            inputShowed: false
+        });
+    },
+    clearInput: function () {
+        this.setData({
+            inputVal: ""
+        });
+    },
+    inputTyping: function (e) {
+        this.setData({
+            inputVal: e.detail.value
+        });
+    },
     searchFriend:function(){
         this.gopage("../friendInfo(simple)/friendInfo(simple)")
     },
