090f4aa7742381433833fe874e744373e13a7afe
Vccxx <18965350261@163.com>
2017-05-24 11:07:13 +0800

    add rsa,aes

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,24 +1,38 @@
 //index.js
 //获取应用实例
+var rsaEnc = require("../../rsa/cryptico.js")
+var aesEnc = require("../../crypto/crypto-js.js")
 var connWebSocket = require("../../function/connect.js")
 var Promise = require("../../lib/Promise.js")
 var app = getApp()
 var scan = '../scanQRCode/scanQRCode'
 var code = '../QRCode/QRCode'
 const self = this
-var trd_recv_state = 1
+var trd_recv_state = 0
 Page({
   data: {
-    encryptedInfo:{},
-    iv :""
   },
   bindChange:function(e){
     this.data.loginInfo[e.currentTarget.id] = e.detail.value
   },
+  onShareAppMessage: function () {
+    return {
+      title: '自定义转发标题',
+      path: '/page/login?id=123',
+      success: function(res) {
+        
+        console.log("show success")
+      },
+      fail: function(res) {
+        // 转发失败
+        console.log("show failed")
+      }
+    }
+  },
   onLoad: function () {
     console.log('onLoad')
      //连接服务器
-    //connWebSocket.connect(this.commonRes,this.commonRej);
+    connWebSocket.connect(this.commonRes,this.commonRej);
     var that = this
     //发送code
     wx.login({
@@ -68,7 +82,6 @@ Page({
           })
         }
       })
-    //获取加密敏感数据
   },
   commonRes:function(result){
       console.log(result)
@@ -76,26 +89,38 @@ Page({
   commonRej:function(result){
       console.log(result)
   },
-  getEncryptedInfo:function(cb){
-    var that = this
-    if(this.data.encryptedInfo && this.data.iv){
-      typeof cb == "function" && cb()
-    }else{
-      wx.getUserInfo({
-        success: function (res) {
-          that.data.encryptedInfo = res.encryptedData
-          that.data.iv = res.iv
-          typeof cb == "function" && cb()
-        }
-      })
-    }
-  },
   sessionKeyRecv:function(res){
     var recv = JSON.parse(res)
-    var result = recv.reply
-    console.log(result)
-    wx.setStorageSync('trd_session_key', result.third)
+    console.log(recv.reply)
+    wx.setStorageSync('trd_session_key', recv.reply)
+    wx.setStorageSync('server_public_key', recv.pubkey)
+    wx.setStorageSync('seq', 0)
     trd_recv_state = 1
+    var pwd = "123456"
+    var data2enc = {
+      trd: res.reply,
+      seq: 0
+    }
+    var textEnc = aesEnc.AES.encrypt(JSON.stringify(data2enc),aesk)
+    var aeskey = textEnc.key.toString()
+    wx.setStorage({
+      key: 'aeskey2server',
+      data: pwd,
+      success:function(){
+        console.log("set aeskey successs")
+      },
+      fail:function(res){
+        console.log(res)
+      }
+    })
+    var aesKeyEnc = rsaEnc.cryptico.encrypt(aeskey, wx.getStorageSync("server_public_key"));
+    console.log(aesKeyEnc.cipher)
+    var dataSent = {
+      state:2,
+      aeskeyEnc: aesKeyEnc.cipher,
+      aesEncText: textEnc
+    } 
+    connWebSocket.sendMsg(dataSent,commonRes,commonRej)
   },
   onPullDownRefresh:function(){
     var that = this
